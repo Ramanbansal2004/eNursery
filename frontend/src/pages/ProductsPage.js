c3ae068e150736aa05c19ea469a7a6699ad0a0e4
Raman <103361956+Ramanbansal2004@users.noreply.github.com>
2025-08-25 16:18:56 +0530

    Optimization

--- frontend/src/pages/ProductsPage.js
+++ frontend/src/pages/ProductsPage.js
@@ -1,20 +1,8 @@
 import Products from '../features/products/Components/Products'
-import { useEffect, useState } from 'react'
-import Animation from '../features/common/Animation';
 const ProductsPage = () => {
-  const[loading, setLoading] = useState(false)
-  useEffect(() => {
-    setTimeout(()=>{
-      setLoading(true);
-    }, 50000)
-  }, [])
   document.title = "Products Page"
   return (
-    <>
-      {!loading && <div>
-        <Animation/></div>}
-      {loading && <Products/>}
-    </>
+    <Products />
   )
 }
 
